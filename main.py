9c2c5286f7f45ad451c35b9d69d1509a2c014f8a
Ruwan Mettananda <ruwanvm@gmail.com>
2021-09-13 10:35:26 +0530

    Add initial version of script

---
+++ main.py
@@ -0,0 +1,4 @@
+# Get Github auth token and Teams webhook URL from AWS secret manager
+# Get repository details from yaml conf file
+# Query open pull requests and get the results
+# If pull request is open more than X days, inform on teams channel
